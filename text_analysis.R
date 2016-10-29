0934c71be1b7950b8249ea62782834b37711287b
James <dayhuffj@gmail.com>
2016-10-29 07:46:55 -0600

    implementing the tm libraryr

--- text_analysis.R
+++ text_analysis.R
@@ -5,10 +5,21 @@ require(RColorBrewer)
 require(tm)
 require(SnowballC)
 require(sentimentr)
+require(qdap)
+
+try.error = function(x)
+{
+  y = NA
+  try_error = tryCatch(tolower(x), error=function(e) e)
+  if (!inherits(try_error, "error"))
+    y = tolower(x)
+  return(y)
+}
 
 tfb = read.csv("trump_fb_analysis.csv")
 
 status_messages = lapply(tfb$status_message, as.character)
+
 head(status_messages)
 str(status_messages)
 class(status_messages)
@@ -23,14 +34,26 @@ status_messages = sapply(status_messages, try.error)
 status_messages = status_messages[!is.na(status_messages)]
 names(status_messages) = NULL
 
-class_emo = classify_emotion(status_messages, algorithm="bayes", prior=1.0)
+### Word clouds ###
+recent_100 = wordcloud(status_messages[1:100], colors=c("red","blue"))
+tail_cloud = wordcloud(tail(status_messages,n=100),colors=c("blue","red"))
 
+## learning the tm library ##
+sub_status_messages = status_messages[1:100]
 
-try.error = function(x)
-{
-  y = NA
-  try_error = tryCatch(tolower(x), error=function(e) e)
-  if (!inherits(try_error, "error"))
-    y = tolower(x)
-  return(y)
-}
+sub_text_all = paste(sub_status_messages, collapse = " ")
+
+text_source = VectorSource(sub_text_all)
+corpus = Corpus(text_source)
+corpus = tm_map(corpus, stripWhitespace)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+
+dtm = DocumentTermMatrix(corpus)
+dtm2 = as.matrix(dtm)
+
+frequency = colSums(dtm2)
+frequency = sort(frequency, decreasing=TRUE)
+head(frequency)
+
+words = names(frequency)
+
